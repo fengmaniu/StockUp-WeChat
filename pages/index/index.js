03a8b5aa2254b3a0ea61b1d166a4df6692fff58d
zgchen <zgchen@3ti.us>
2016-11-14 21:44:31 +0800

    swidder

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,24 +3,37 @@
 var app = getApp()
 Page({
   data: {
-    motto: 'Hello Guzhang2',
-    userInfo: {}
+    background: ['green', 'red', 'yellow'],
+    indicatorDots: true,
+    vertical: false,
+    autoplay: true,
+    interval: 3000,
+    duration: 1200
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
+  changeIndicatorDots: function (e) {
+    this.setData({
+      indicatorDots: !this.data.indicatorDots
     })
   },
-  onLoad: function () {
-    console.log('onLoad')
-    var that = this
-    //调用应用实例的方法获取全局数据
-    app.getUserInfo(function(userInfo){
-      //更新数据
-      that.setData({
-        userInfo:userInfo
-      })
+  changeVertical: function (e) {
+    this.setData({
+      vertical: !this.data.vertical
+    })
+  },
+  changeAutoplay: function (e) {
+    this.setData({
+      autoplay: !this.data.autoplay
+    })
+  },
+  intervalChange: function (e) {
+    this.setData({
+      interval: e.detail.value
+    })
+  },
+  durationChange: function (e) {
+    this.setData({
+      duration: e.detail.value
     })
   }
 })
+
