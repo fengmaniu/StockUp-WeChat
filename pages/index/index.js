e43db5f06dda31f6c7339c20278c7d0630b37d0e
zgchen <zgchen@3ti.us>
2016-11-16 20:54:05 +0800

    list

--- pages/index/index.js
+++ pages/index/index.js
@@ -8,7 +8,48 @@ Page({
     vertical: false,
     autoplay: true,
     interval: 3000,
-    duration: 1200
+    duration: 1200,
+    list: [
+      {
+        id: 'view',
+        name: '视图容器',
+        open: false,
+        pages: ['view', 'scroll-view', 'swiper']
+      }, {
+        id: 'content',
+        name: '基础内容',
+        open: false,
+        pages: ['text', 'icon', 'progress']
+      }, {
+        id: 'form',
+        name: '表单组件',
+        open: false,
+        pages: ['button', 'checkbox', 'form', 'input', 'label', 'picker', 'radio', 'slider', 'switch']
+      }, {
+        id: 'feedback',
+        name: '操作反馈',
+        open: false,
+        pages: ['action-sheet', 'modal', 'toast', 'loading']
+      }, {
+        id: 'nav',
+        name: '导航',
+        open: false,
+        pages: ['navigator']
+      }, {
+        id: 'media',
+        name: '媒体组件',
+        open: false,
+        pages: ['image', 'audio', 'video']
+      }, {
+        id: 'map',
+        name: '地图',
+        pages: ['map']
+      }, {
+        id: 'canvas',
+        name: '画布',
+        pages: ['canvas']
+      }
+    ]
   },
   changeIndicatorDots: function (e) {
     this.setData({
