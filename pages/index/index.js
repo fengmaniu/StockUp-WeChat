0d7ddcdc880ed3c6f6af4f84876391c3312957a1
zgchen <zgchen@3ti.us>
2016-11-26 20:35:45 +0800

    update

--- pages/index/index.js
+++ pages/index/index.js
@@ -12,43 +12,23 @@ Page({
     image:['/images/test1.jpg','/images/test2.jpg','/images/test3.jpg'],
     list: [
       {
-        id: 'view',
-        name: '视图容器',
-        open: false,
-        pages: ['view', 'scroll-view', 'swiper']
+        id: 'stack1',
+        name: '海天精工',
+        trend:'涨0.01%',
+        image:'/images/test1.jpg',
+        // open: false,
+        // pages: ['view', 'scroll-view', 'swiper']
       }, {
-        id: 'content',
-        name: '基础内容',
-        open: false,
-        pages: ['text', 'icon', 'progress']
+        id: 'stack2',
+        name: '南洋科技',
+        trend:'涨0.05%',
+        image:'/images/test2.jpg',
+
       }, {
-        id: 'form',
-        name: '表单组件',
-        open: false,
-        pages: ['button', 'checkbox', 'form', 'input', 'label', 'picker', 'radio', 'slider', 'switch']
-      }, {
-        id: 'feedback',
-        name: '操作反馈',
-        open: false,
-        pages: ['action-sheet', 'modal', 'toast', 'loading']
-      }, {
-        id: 'nav',
-        name: '导航',
-        open: false,
-        pages: ['navigator']
-      }, {
-        id: 'media',
-        name: '媒体组件',
-        open: false,
-        pages: ['image', 'audio', 'video']
-      }, {
-        id: 'map',
-        name: '地图',
-        pages: ['map']
-      }, {
-        id: 'canvas',
-        name: '画布',
-        pages: ['canvas']
+        id: 'stack3',
+        name: '华新股份',
+        trend:'跌0.05%',
+        image:'/images/test3.jpg',
       }
     ]
   },
