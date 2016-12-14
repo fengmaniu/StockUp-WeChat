d9b205b553153da304589d665092761af8d94d4e
Landary <625406011@qq.com>
2016-12-14 23:10:51 +0800

    haokaisen

--- pages/personal/personal.js
+++ pages/personal/personal.js
@@ -6,6 +6,12 @@ Page({
     mode:'aspectFit',
     list:'Landary',
     title:"今日收益：",
-    money:"666.66"
+    money:"666.66",
+    defaultSize: 'default',
+    primarySize: 'default',
+    warnSize: 'default',
+    disabled: false,
+    plain: false,
+    loading: false
   }
 })
